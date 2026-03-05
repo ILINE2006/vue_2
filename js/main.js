7896104565bc92094edad25fb510be444c6d06f6
ILINE2006 <teplotom71@gmail.com>
2026-03-05 16:32:47 +0700

    Добавлен компонент столбца сделан через v-for и некоторые стили

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
+Vue.component('note-column', {
+  props: {
+    column: {
+      type: Object,
+      required: true
+    }
+  },
+  template: `
+    <div class="column">
+      <h2>{{ column.name }}</h2>
+      <p>Карточки: {{ column.cards.length }} / {{ column.maxCards || 'бесконечно' }}</p>
+    </div>
+  `
+})
+
 let app = new Vue({
   el: '#app',
-  data: {
+   data: {
     columns: [
-      { id: 1, name: 'Блок_1', maxCards: 3, cards: [] },
-      { id: 2, name: 'Блок_2', maxCards: 5, cards: [] },
-      { id: 3, name: 'Блок_3', maxCards: null, cards: [] }
+      { id: 1, name: 'Блок 1', maxCards: 3, cards: [] },
+      { id: 2, name: 'Блок 2', maxCards: 5, cards: [] },
+      { id: 3, name: 'Блок 3', maxCards: null, cards: [] }
     ]
   }
 })
